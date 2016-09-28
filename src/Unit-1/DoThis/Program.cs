96e84de25e066e79dcf81b023b4a842c19e48bb0
Paul Tsai <paultsai@gmail.com>
2016-09-28 18:14:57 -0400

    unit 1 lesson 2

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -10,36 +10,20 @@ namespace WinTail
 
         static void Main(string[] args)
         {
+            // make an actor system 
             MyActorSystem = ActorSystem.Create("MyActorSystem");
-            PrintInstructions();
 
-            // time to make your first actors!
+            // make our first actors!
             var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()),
                 "consoleWriterActor");
             var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)),
                 "consoleReaderActor");
 
             // tell console reader to begin
-            consoleReaderActor.Tell("start");
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
-        }
-
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
         }
     }
     #endregion
