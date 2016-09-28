cb4be4a0b8662e24aa81f3c40b0222461392790f
Paul Tsai <paultsai@gmail.com>
2016-09-28 18:45:42 -0400

    Unit 1 Lesson 3a

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -13,6 +13,14 @@ namespace WinTail
             // make an actor system 
             MyActorSystem = ActorSystem.Create("MyActorSystem");
 
+            // this is here to show you what NOT to do
+            // this approach to props has no type safety
+            // it will compile, but can easily blow up in your face at runtime :(
+            // UNCOMMENT THE BELOW TWO LINES, BUILD THE SOLUTION, AND THEN TRY TO RUN IT TO SEE
+            //Props fakeActorProps = Props.Create(typeof(FakeActor));
+            //IActorRef fakeActor = MyActorSystem.ActorOf(fakeActorProps, "fakeActor");
+
+            // set up actors, using props (split props onto own line so easier to read)
             var consoleWriterProps = Props.Create<ConsoleWriterActor>();
             var consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
             var validationActorProps = Props.Create(() => new ValidationActor(consoleWriterActor));
@@ -25,6 +33,13 @@ namespace WinTail
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
         }
+
+        /// <summary>
+        /// Fake actor / marker class. Does nothing at all, and not even an actor actually. 
+        /// Here to show why you shouldn't use typeof approach to Props.
+        /// </summary>
+        public class FakeActor {}
+
     }
     #endregion
 }
